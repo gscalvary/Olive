391da5a6a87222fa6ec92cebf345a8e900033f53
gscalvary <Christopher@Christopher-Olivers-MacBook-Pro-3.local>
2014-12-03 14:59:13 -0500

    Test the commit to Github.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Olive
+//
+//  Created by Christopher Oliver on 12/3/14.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
