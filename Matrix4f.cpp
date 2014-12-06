97dd426bc942028c08008b75c71a3509e72702bc
gscalvary <Christopher@Christopher-Olivers-MacBook-Pro-3.local>
2014-12-06 18:07:36 -0500

    New matrix class with its associated mathematics.

---
+++ Matrix4f.cpp
@@ -0,0 +1,61 @@
+//
+//  Matrix4f.cpp
+//  Olive
+//
+//  Created by Christopher Oliver on 12/6/14.
+//
+//
+
+#include "Matrix4f.h"
+
+Matrix4f::Matrix4f() {
+    
+    for (unsigned i = 0; i < 4; ++i) {
+        for (unsigned j = 0; j < 4; ++j) {
+            m[i][j] = 0.0;
+        }
+    }
+}
+
+float Matrix4f::getEntryMatrix4f(unsigned i, unsigned j) {
+    
+    if (i >= 4 || j >= 4) {
+        return 0.0;
+    }
+    
+    return m[i][j];
+}
+
+void Matrix4f::setEntryMatrix4f(unsigned i, unsigned j, float newValue) {
+    
+    if (i >= 4 || j >= 4) {
+        return;
+    }
+    
+    m[i][j] = newValue;
+}
+
+void Matrix4f::setIdentityMatrix4f() {
+    
+    m[0][0] = 1.0;    m[0][1] = 0.0;   m[0][2] = 0.0;   m[0][3] = 0.0;
+    m[1][0] = 0.0;    m[1][1] = 1.0;   m[1][2] = 0.0;   m[1][3] = 0.0;
+    m[2][0] = 0.0;    m[2][1] = 0.0;   m[2][2] = 1.0;   m[2][3] = 0.0;
+    m[3][0] = 0.0;    m[3][1] = 0.0;   m[3][2] = 0.0;   m[3][3] = 1.0;
+}
+
+Matrix4f Matrix4f::multIdentityMatrix4f(Matrix4f n) {
+    
+    Matrix4f* res = new Matrix4f();
+    
+    for (unsigned i; i < 4; ++i) {
+        for (unsigned j; j < 4; ++j) {
+            res->setEntryMatrix4f(i, j,
+                                  m[i][0] * n.getEntryMatrix4f(0, j) +
+                                  m[i][1] * n.getEntryMatrix4f(1, j) +
+                                  m[i][2] * n.getEntryMatrix4f(2, j) +
+                                  m[i][3] * n.getEntryMatrix4f(3, j));
+        }
+    }
+    
+    return *res;
+}
---
+++ Matrix4f.h
@@ -0,0 +1,74 @@
+//
+//  Matrix4f.h
+//  Olive
+//
+//  Created by Christopher Oliver on 12/6/14.
+//
+//
+
+#ifndef __Olive__Matrix4f__
+#define __Olive__Matrix4f__
+
+#include <stdio.h>
+
+#endif /* defined(__Olive__Matrix4f__) */
+
+class Matrix4f {
+public:
+    
+    //  -> Matrix4f*
+    // Default constructor for a Matrix4f object.
+    // Example: Matrix4f(); builds a matix with all components = 0.0.
+    // Strategy: Domain Knowledge
+    Matrix4f();
+    
+    // unsigned, unsigned -> float
+    // Given a horizontal and a vertical index return the value of the
+    // indexed component.
+    // INVARIANTS: i and j are < 4
+    // Example: getEntryMatrix4f(0, 0); returns m[0][0].
+    // Strategy: Domain Knowledge
+    float getEntryMatrix4f(unsigned i, unsigned j);
+    
+    // int, int, float -> void
+    // EFFECT: Given a horizontal and a vertical index set the value of the
+    // indexed component to the given float.
+    // INVARIANTS: i and j are < 4
+    // Example: setEntryMatrix4f(0, 0, 1.0); sets m[0][0] = 1.0.
+    // Strategy: Domain Knowledge
+    void setEntryMatrix4f(unsigned i, unsigned j, float newValue);
+    
+    //  -> void
+    // EFFECT: Set the matrix to the identity matrix.
+    // Example: setIdentityMatrix4f(); sets:
+    //  m[0][0] = 1.0    m[0][1] = 0.0   m[0][2] = 0.0   m[0][3] = 0.0
+    //  m[1][0] = 0.0    m[1][1] = 1.0   m[1][2] = 0.0   m[1][3] = 0.0
+    //  m[2][0] = 0.0    m[2][1] = 0.0   m[2][2] = 1.0   m[2][3] = 0.0
+    //  m[3][0] = 0.0    m[3][1] = 0.0   m[3][2] = 0.0   m[3][3] = 1.0
+    // Strategy: Domain Knowledge
+    void setIdentityMatrix4f();
+    
+    // Matrix4f -> Matrix4f
+    // Given a matrix multiply it by this matrix and return the result.
+    // Example: myMatrix =
+    //  m[0][0] = 1.0    m[0][1] = 0.0   m[0][2] = 0.0   m[0][3] = 0.0
+    //  m[1][0] = 2.0    m[1][1] = 1.0   m[1][2] = 0.0   m[1][3] = 0.0
+    //  m[2][0] = 3.0    m[2][1] = 2.0   m[2][2] = 1.0   m[2][3] = 0.0
+    //  m[3][0] = 4.0    m[3][1] = 3.0   m[3][2] = 2.0   m[3][3] = 1.0)
+    //  multMatrix4f(
+    //  m[0][0] = 1.0    m[0][1] = 0.0   m[0][2] = 0.0   m[0][3] = 0.0
+    //  m[1][0] = 2.0    m[1][1] = 1.0   m[1][2] = 0.0   m[1][3] = 0.0
+    //  m[2][0] = 3.0    m[2][1] = 2.0   m[2][2] = 1.0   m[2][3] = 0.0
+    //  m[3][0] = 4.0    m[3][1] = 3.0   m[3][2] = 2.0   m[3][3] = 1.0)
+    //  returns:
+    //  m[0][0] = 1.0    m[0][1] = 0.0   m[0][2] = 0.0   m[0][3] = 0.0
+    //  m[1][0] = 0.0    m[1][1] = 1.0   m[1][2] = 0.0   m[1][3] = 0.0
+    //  m[2][0] = 0.0    m[2][1] = 0.0   m[2][2] = 1.0   m[2][3] = 0.0
+    //  m[3][0] = 0.0    m[3][1] = 0.0   m[3][2] = 0.0   m[3][3] = 1.0)
+    // Strategy: Domain Knowledge
+    Matrix4f multIdentityMatrix4f(Matrix4f n);
+    
+private:
+    
+    float m[4][4];
+};
